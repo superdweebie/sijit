be2a07d22f2ce3de9b27fa21c77eaa90a43b4cf6
Tim Roediger <tim.roediger@ausaero.com.au>
2012-08-02 16:20:40 +1000

    Added jslink widget

---
+++ Common/JsLink.js
@@ -0,0 +1,25 @@
+define([
+    'dojo/_base/declare',
+    'dijit/_Widget',
+    'dijit/_TemplatedMixin',
+    'dijit/_OnDijitClickMixin',
+    'dojo/text!./Template/JsLink.html',
+],
+function(
+    declare,
+    Widget,
+    TemplatedMixin,
+    OnDijitClickMixin,
+    template
+){
+    return declare
+    (
+        'sijit.Common.JsLink',
+        [Widget, TemplatedMixin, OnDijitClickMixin],
+        {
+            templateString: template,
+            onClick: function(){}
+        }
+    );
+});
+
