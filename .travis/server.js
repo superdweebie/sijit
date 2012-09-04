e84eb334981a908716c24f7a3c62f1f3d9323bd6
Tim Roediger <superdweebie@gmail.com>
2012-09-05 09:41:09 +1000

    Update .travis/server.js

--- .travis/server.js
+++ .travis/server.js
@@ -1,4 +1,10 @@
-var connect = require('connect');
-connect.createServer(
-    connect.static(__dirname)
-).listen(80);
+var connect = require('connect'),
+args = process.argv.slice(2),
+folder = args[0] || '/../',
+port = args[1] || '80';    
+
+var server = connect.createServer(
+    connect.static(__dirname + folder)
+).listen(port);
+
+console.log("Server started on port %s in %s", port, folder);
